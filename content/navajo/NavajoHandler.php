9fe73ad7c68dc6e8a6380b37527a9074ffdbdbde
Frank Lyaruu <frank@dexels.com>
2008-02-15 12:06:18 +0000

    fixed session handling

--- content/navajo/NavajoHandler.php
+++ content/navajo/NavajoHandler.php
@@ -1,6 +1,17 @@
 <?php
-
+$joomSess = session_name();
+session_write_close();;
+session_name($_REQUEST[$joomSess]);
+require_once "client/NavajoClient.php";
+require_once "document/NavajoDoc.php";
+require_once "phpclient/NavajoPhpClient.php";
+session_start();
 NavajoClient :: updateNavajoFromPost();
+
+session_write_close();;
+session_name($joomSess);
+session_start();
+
 
 # based on the label of the submit, get correct id
 if (isset ($_REQUEST['submit'])) {
@@ -35,7 +46,7 @@ if (isset ($_REQUEST['serverCall']) && isset($_SESSION['formId'])) {
     }
 }
 
-/*
+
 if (isset($target) || isset ($_REQUEST['action'])) {
     if ($_REQUEST['action'] == 'exit') {
         if (isset ($_SESSION['site'])) {
@@ -53,7 +64,7 @@ if (isset($target) || isset ($_REQUEST['action'])) {
         include $_SERVER['DOCUMENT_ROOT'] . $siteHome . $_SESSION['currentPage'] . '.php';
     }
 }
-*/
+
 # set a new form secret for the next form
 if(!isset($_SESSION['formId'])) {
     $secret = md5(uniqid(rand(), true));
